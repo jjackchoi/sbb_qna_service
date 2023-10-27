3b1ee7099166a3d688f438bed9af6315fb1ed5bc
jjackchoi <jackchoi0325@gmail.com>
2023-10-27 17:31:22 +0900

    plus minus 구현

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -1,7 +1,9 @@
 package com.exam.sbb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,4 +16,17 @@ public class MainController {
         return "안녕하세요~";
     }
 
+    @GetMapping("/plus")
+    @ResponseBody
+    public int showPlus(int a, int b){
+        return a+b;
+
+    }
+
+    @GetMapping("/minus")
+    @ResponseBody
+    public int showMinus(@RequestParam(defaultValue = "0")int a, int b){
+        return a-b;
+
+    }
 }
