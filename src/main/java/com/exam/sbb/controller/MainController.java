f8fa1359c6152aec0ec4b909a5cf854b8fbc91e8
jjackchoi <jackchoi0325@gmail.com>
2023-10-27 16:51:25 +0900

    ResponseBody 설명

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
     @RequestMapping("/sbb")
+    //아래 함수의 리턴값을 그대로 브라우저에 표시
+    //아래 함수의 리턴값을 문자열(String)화 해서 브라우저에 표시
     @ResponseBody
     public String index(){
         return "안녕하세요~";
